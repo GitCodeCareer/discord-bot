f42dafa52c9712eb4258c45f3192ac5899ac27a5
ap4gh <pandey.amrit@outlook.com>
2019-04-10 17:04:11 +0530

    all events are now cached at the beginning of the program

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const moduleAlias = require('module-alias');
+require('colors');
 
+const moduleAlias = require('module-alias');
 moduleAlias.addAliases({
-  '@root'  : __dirname,
+  '@root': __dirname,
   '@utils': __dirname + '/utils',
   '@events': __dirname + '/events',
   '@commands': __dirname + '/commands'
 });
 
-const Config = require('./utils/config');
+const Config = require('@utils/config');
+const EventHandler = require('./events/EventHandler');
+const eventList = require('./configurations/events');
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client } = require('discord.js');
+const client = new Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+/**
+ * CACHING: Cache all the events on the first
+ * run of the program. This reduces response
+ * time and save hassle of importing modules
+ * everytime.
+ */
 
-const fs = require('fs');
-
-fs.readdir('./events/', (err, files) => {
-  files.forEach((file) => {
-    let eventName = file.split('.')[0];
-    let eventFile = require(`./events/${file}`);
-
-    client.on(eventName, (object) => eventFile.run(object));
-  });
-});
+const eventHandler = new EventHandler(client);
+// load up all the existing events in cache
+eventHandler.load();
+// register all event handlers with client
+for (const e of Object.keys(eventList)) {
+  client.on(e, (...args) => eventHandler.onEvent(e, ...args));
+}
 
 client.login(Config.getBotToken());
 
 module.exports = {
   client
-}
+};
