374bdcf731a53fd26d1c067480d9adf55bcf2027
Alex Crocker <hello@crock.dev>
2019-06-27 13:19:43 -0400

    Firebase Integration (#25)
    * Added firebase integration
    Added additional admin configuration commands
    
    * Deleted old json config
    Changed node version in pm2 conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       {
         name      : 'cc-bot',
         script    : 'index.js',
-        interpreter: 'node@10.15.3',
+        interpreter: 'node@10.16.0',
         env: {
           NODE_ENV: "development"
         },
