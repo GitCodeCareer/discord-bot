b322c57ed94a3c0b42289c6dc4473ca3c25cf050
Amrit Pandey <amrit@pop-os.localdomain>
2019-08-12 16:19:38 +0530

    some formatting

--- commands/announce.js
+++ commands/announce.js
@@ -1,14 +1,16 @@
 const Auth = require('../utils/auth')
 
-exports.run = (message, args) => {
+exports.run = async (message, args) => {
    
    if (Auth.isAdmin(message.member)) {
-      if(!args || args.length < 1) return message.reply("Must provide the text for the announcement after the command.");
+      
+      if(!args || args.length < 1) 
+         return message.reply("Must provide the text for the announcement after the command.");
 
       const announcement = args.join(' ');
 
-      message.channel.send(announcement);
-      message.delete();
+      await message.channel.send(announcement);
+      await message.delete();
    } else {
       message.reply('You must be an admin in order to run this command.');
    }
