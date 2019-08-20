026944ddc0ea11f0576cd2474f3d53ef63d4ac31
Alex Crocker <hello@crock.dev>
2019-08-20 12:40:08 -0400

    Added header images locally

---
+++ utils/storage.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const fs = require('fs')
+
+class Storage {
+
+}
+
+module.exports 
--- utils/view.js
+++ utils/view.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const fs = require('fs')
 const handlebars = require('handlebars')
-const { Response } = require('http')
 
 class View {
     constructor(name, variables, email) {
