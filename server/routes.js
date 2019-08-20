bce32e547cbbf6caef30f871c0c954083e49cbc9
Alex Crocker <hello@crock.dev>
2019-08-20 14:17:31 -0400

    Added basic route middleware functionality

--- server/routes.js
+++ server/routes.js
@@ -4,12 +4,14 @@ const { view } = require('../utils')
 module.exports = [
     {
         path: `/`,
+        middleware: ['web'],
         method: () => {
             return view('index', { test: "hello world" });
         }
     },
     {
         path: `/member-count`,
+        middleware: ['api'],
         method: () => {
             return {
                 memberCount: client.guilds.first().memberCount
