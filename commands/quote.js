d9bd9cd45a8b42c10523e27ebe95bb9d8fdf4cc5
Thomas Erhel <thomas.erhel@gmail.com>
2019-10-09 12:49:13 +0200

    Added quote command, node-fetch&updated deps

--- commands/quote.js
+++ commands/quote.js
@@ -1,27 +1,24 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 
-
 exports.run = async (message) => {
-const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-const { quote } = await fetch('http://quotes.stormconsultancy.co.uk/random.json').then(response => response.json())
-const { author } = await fetch('http://quotes.stormconsultancy.co.uk/random.json').then(response => response.json())
+const { quote, author } = await fetch('http://quotes.stormconsultancy.co.uk/random.json').then(response => response.json())
+const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json())
 const { url } = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(response => response.json())
 
 // inside a command, event listener, etc.
 const quoteEmbed = new Discord.RichEmbed()
 	.setColor('#5DBCD2')
+	.setAuthor(author, 'https://robohash.org/CodeCareer.io.png')
+	.setThumbnail(url)
 	.setTitle('Motivational Quote 💡🎯🚀')
 	.setURL('https://codecareer.io/')
-	.setAuthor(author, 'https://robohash.org/CodeCareer.io.png')
 	.setDescription(quote)
-	.setThumbnail('https://avatars3.githubusercontent.com/u/42856887?s=200&v=4')
     .addField('🎧 Baroque Music for Studying & Brain Power', 'https://open.spotify.com/playlist/2xwP2mUA0QRT5TwMEkBvtH', true)
 	.addField('🎥 Motivation2Study', 'https://www.youtube.com/channel/UC8PICQUP0a_HsrA9S4IIgWw', true)
-	.addField('🔭 Astronomy Picture of the Day', url)
 	.setImage(file)
+    .setFooter('CodeCareer is committed to helping new developers make their first PR!', 'https://avatars3.githubusercontent.com/u/42856887?s=200&v=4')
 	.setTimestamp()
-    .setFooter('CodeCareer is committed to helping new developers make their first PR!', 'https://avatars3.githubusercontent.com/u/42856887?s=200&v=4');
 
 message.channel.send(quoteEmbed);
 };
