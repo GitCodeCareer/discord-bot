58d4ea6d7427de021a88b918e9a99cb69aa2fc2b
Thomas Erhel <thomas.erhel@gmail.com>
2019-10-14 21:16:36 +0200

    Updated quote command, Deps, Docs. Added zlib & zucc (#39)
    * Updated quote command, Deps, Docs. Added zlib & zucc
    
    * Updated quote command, .env.example Removed Deps.

--- commands/quote.js
+++ commands/quote.js
@@ -3,20 +3,24 @@ const fetch = require('node-fetch');
 
 exports.run = async (message) => {
 const { quote, author } = await fetch('http://quotes.stormconsultancy.co.uk/random.json').then(response => response.json())
-const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json())
-const { url } = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(response => response.json())
+const { contents } = await fetch('https://quotes.rest/qod.json').then(response => response.json())
+const { data } = await fetch(process.env.GIPHY_API_KEY).then(response => response.json())
+const { url } = await fetch(process.env.NASA_API_KEY).then(response => response.json())
 
 // inside a command, event listener, etc.
 const quoteEmbed = new Discord.RichEmbed()
     .setColor('#5DBCD2')
     .setAuthor(author, 'https://robohash.org/CodeCareer.io.png')
     .setThumbnail(url)
-    .setTitle('Motivational Quote 💡🎯🚀')
+    .setTitle('💡 Motivational Quote 🎯🚀')
     .setURL('https://codecareer.io/')
     .setDescription(quote)
-    .addField('🎧 Baroque Music for Studying & Brain Power', 'https://open.spotify.com/playlist/2xwP2mUA0QRT5TwMEkBvtH', true)
-    .addField('🎥 Motivation2Study', 'https://www.youtube.com/channel/UC8PICQUP0a_HsrA9S4IIgWw', true)
-    .setImage(file)
+    .addField('🎧 Podcast :', 'https://anchor.fm/codecareer', true)
+    .addField('🌠 Quote Of The Day :', contents.quotes[0].author, true)
+    .addField(contents.quotes[0].quote, contents.quotes[0].tags, true)
+    .addField('📹 YouTube :', 'https://www.youtube.com/user/bnspak/', true)
+    .addField('🔮 Random GIF :', data.title, true)
+    .setImage(data.image_url)
     .setFooter('CodeCareer is committed to helping new developers make their first PR!', 'https://avatars3.githubusercontent.com/u/42856887?s=200&v=4')
     .setTimestamp()
 
