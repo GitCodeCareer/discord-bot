f5bca758fedd6bc97ea6736bb082e0ef8377abe0
Glitch (codecareer-bot) <none>
2020-04-03 17:10:43 +0000

    🌑🏓 Updated with Glitch

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const bot = require('../utils/client').getClient()
 exports.run = async function () {
   console.log(`Logged in as ${bot.user.tag.black.bgYellow}`);
   // set activity
-  const presense = await bot.user.setActivity("Type !ticket for support");
+  const presense = await bot.user.setActivity("codecareer.io");
   console.log(`Activity set to ${presense.game ? presense.game.name.black.bgYellow : "none"}`)
 }
